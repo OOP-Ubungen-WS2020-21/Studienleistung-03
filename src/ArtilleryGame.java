eec74136938a176e32bbb77514d727a6df5e7b49
Jürgen Hahn <eth3real89@gmail.com>
2020-12-10 10:41:08 +0100

    added Startpaket for Studienleistung 3

---
+++ src/ArtilleryGame.java
@@ -0,0 +1,19 @@
+import de.ur.mi.oop.app.GraphicsApp;
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+
+public class ArtilleryGame extends GraphicsApp {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    public static void main(String[] args) {
+        GraphicsAppLauncher.launch();
+    }
+}
